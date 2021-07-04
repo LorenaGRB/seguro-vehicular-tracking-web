a4bbfce7b8b1ed53d5e2ca04258eb6bbaedc0a67
Lorena Rojas <lorenagabby4@gmail.com>
2021-07-03 22:55:38 -0500

    creation of header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Layout from './components/Layout'
+
 function App() {
   return (
     <div className="App">
-
+      <Layout />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../../assets/images/header/logo.svg'
+import classes from '../../style/style.module.css'
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <img src={logo} alt='logo'/>
+            <p className={classes.header__callus}>Llámanos</p>
+            <div className={classes.header__desktop}>
+                <p className={classes.header__question}>¿Tienes alguna duda?</p>
+                <p className={classes.header__numberPhone}>(01) 411 6001</p>
+            </div>
+        </header>
+    )
+}
+export default Header

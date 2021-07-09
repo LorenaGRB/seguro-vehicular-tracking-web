d5299172911db80eba8f1d69f94151c8a3846d3d
Lorena Rojas <lorenagabby4@gmail.com>
2021-07-08 21:00:59 -0500

    Title and select components are created with their styles

--- src/components/CarData/CarDataForm/CarDataForm.js
+++ src/components/CarData/CarDataForm/CarDataForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import Select from '../../UI/Select'
 import Input from '../../UI/Input'
+import Button from '../../UI/Button'
 import classes from '../../../style/style.module.css'
 import submitHandler from '../../../functions/validationFormHome'
+import iconCar from "../../../assets/images/datosAuto/icon-car.svg";
 
 function CarDataForm(props) {
 
@@ -18,47 +20,76 @@ function CarDataForm(props) {
     const [onGas, setonGas] = useState('')
     return (
         <section className={classes.carDataForm}>
-            <h2 className={classes.carDataForm__title}>¡Hola, Meyling!</h2>
+            <h2 className={classes.carDataForm__title}>¡Hola, <span>Meyling!</span></h2>
             <h3 className={classes.carDataForm__subtitle}>Completa los datos de tu auto </h3>
             <form className={classes.carDataForm__form} onSubmit={submitHandler}>
-                <div>
-                    
-                    <Select
-                    id='carYear'
-                    component='carDataForm'
-                    label = 'Año'
-                    options={optionsOfYear}
-                    value={carYear}
-                    onchange={e=>{setcarYear(e.target.value)}}
-                />
-                    <Select
-                    id='carBrand'
-                    component='carDataForm'
-                    label = 'Marca'
-                    options={listBrandCar}
-                    value={carBrand}
-                    onchange={e=>{setcarBrand(e.target.value)}}
-                />
+                <div className={classes[`carDataForm__select`]}>
+                    <div>
+                        <Select
+                        id='carYear'
+                        component='carDataForm'
+                        label = 'Año'
+                        options={optionsOfYear}
+                        value={carYear}
+                        onchange={e=>{setcarYear(e.target.value)}}
+                        />
+
+                        <Select
+                        id='carBrand'
+                        component='carDataForm'
+                        label = 'Marca'
+                        options={listBrandCar}
+                        value={carBrand}
+                        onchange={e=>{setcarBrand(e.target.value)}}
+                        />
+                    </div>
+                    <div className={classes.carDataForm__help}>
+                        <div>
+                            <p className={classes[`carDataForm__help-title`]}>AYUDA</p>
+                            <p className={classes[`carDataForm__help-question`]}>¿No encuentras el modelo?</p>
+                            <a className={classes[`carDataForm__help-link`]} href="./">clic aquí</a>
+                        </div>
+                        <img className={classes[`carDataForm__help-car`]} src={iconCar} alt='Carro'/>
+                    </div>
                 </div>
                 <div>
-                    <label>¿Tu auto es a gas?</label>
-                    <Input 
-                    id='onGas'
-                    type='radio'
-                    label='Si'
-                    component='carDataForm'
-                    value={onGas}
-                    onclick={e=>{setonGas(!onGas)}}
-                    />
-                    <Input 
-                        id='onGas'
+                    <label className={classes[`carDataForm__radio-title`]}>¿Tu auto es a gas?</label>
+                    <div className={classes[`carDataForm__radio`]}>
+                        <Input 
+                        id='withGas'
                         type='radio'
-                        label='No'
+                        label='Si'
                         component='carDataForm'
                         value={onGas}
                         onclick={e=>{setonGas(!onGas)}}
-                    />
+                        />
+                        <Input 
+                            id='withoutGas'
+                            type='radio'
+                            label='No'
+                            component='carDataForm'
+                            value={onGas}
+                            onclick={e=>{setonGas(!onGas)}}
+                        />
+                    </div>
                 </div>
+                <div className={classes.carDataForm__buttons}>
+                    <p className={classes.carDataForm__buttons}>Indica la suma asegurada</p>
+                    <div>
+                        <p>MIN $12.500</p>
+                        <p>MAX $16,500</p>
+                    </div>
+                    <div>
+                        <button>-</button>
+                        <p>$16,500</p>
+                        <button>+</button>
+                    </div>
+                </div>
+                <Button
+                    id='continuar'
+                    component='carDataForm'
+                    html='Continuar'
+                />
             </form>
         </section>
     )
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -4,7 +4,11 @@ import classes from '../../style/style.module.css'
 function Button(props) {
     
     return (
-        <button type='submit' className={classes[`${props.component}__${props.id}-button`]}>
+        <button
+            type='submit' 
+            dangerouslySetInnerHTML={{ __html: props.html }}
+            className={classes[`${props.component}__${props.id}-button`]}
+        >
             {props.text}
         </button>
     )
